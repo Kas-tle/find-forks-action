8a78e71d572f1c31c426aaee73d6eae50bdff7b1
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2023-02-05 20:57:12 -0800

    Deal with PRs correctly
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- index.js
+++ index.js
@@ -9,20 +9,17 @@ try {
     const eventPath = process.env.GITHUB_EVENT_PATH;
     const event = JSON.parse(fs.readFileSync(eventPath, "utf-8"));
     let repositoryOwner = process.env.GITHUB_REPOSITORY.split("/")[0];
+    let repositoryName = process.env.GITHUB_REPOSITORY.split("/")[1];
     if (event.pull_request) {
         repositoryOwner = event.pull_request.head.repo.owner.login;
+        repositoryName = event.pull_request.head.ref;
     }
 
     const owner = core.getInput('owner', {required: false}) || currentOwner;
     const repo = core.getInput('repo', {required: false}) || currentRepo;
     const targetUser = core.getInput('target_user', {required: false}) || repositoryOwner;
-    const targetBranch = core.getInput('target_branch', {required: false}) || process.env.GITHUB_REF.split("/").slice(-1)[0];
+    const targetBranch = core.getInput('target_branch', {required: false}) || repositoryName;
     const githubToken = core.getInput('token', {required: true});
-
-    let checkBranch = true;
-    if (targetBranch === "") {
-        checkBranch = false;
-    }
 
     const options = {
         hostname: "api.github.com",
@@ -57,21 +54,19 @@ try {
                         foundFork = true;
                         userFork = fork.html_url;
                         core.setOutput("user_fork_url", userFork);
-                        if (checkBranch) {
-                            options.path = `/repos/${fork.full_name}/branches/${targetBranch}`;
-                            https.get(options, res => {
-                                if (res.statusCode === 200) {
-                                    core.info(`Found match for user "${targetUser}" with branch "${targetBranch}" at URL: ${userFork}`);
-                                    found = true;
-                                    core.setOutput("target_branch_found", true);
-                                    return;
-                                } else if (res.statusCode === 404) {
-                                    core.info(`Branch "${targetBranch}" not found for user "${targetUser}"`);
-                                    core.setOutput("target_branch_found", false);
-                                    return;
-                                }
-                            });
-                        }
+                        options.path = `/repos/${fork.full_name}/branches/${targetBranch}`;
+                        https.get(options, res => {
+                            if (res.statusCode === 200) {
+                                core.info(`Found match for user ${targetUser} with branch ${targetBranch} at URL ${userFork}`);
+                                found = true;
+                                core.setOutput("target_branch_found", true);
+                                return;
+                            } else if (res.statusCode === 404) {
+                                core.info(`Found match for user ${targetUser} at URL ${userFork}`);
+                                core.setOutput("target_branch_found", false);
+                                return;
+                            }
+                        });
                         return false;
                     }
                     return true;
